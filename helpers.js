0823815f1fcc16fa8ed59b9bbde761bf99576cf2
Yiling Chen <yilingchen1@gmail.com>
2016-03-12 17:44:59 -0800

    removed unecessary helpers

--- helpers.js
+++ helpers.js
@@ -11,44 +11,11 @@ function include(array, item) {
   }
 }
 
-function stringInclude(big, small) {
-  for (var i = 0; i < big.length; i++) {
-    for (var j = 1; j <= big.length; j++) {
-      if (big.slice(i, j) === small) {
-        return true
-      }
-    }
-  }
-  return false
-}
-
 function outsideRange(n) {
   if (n < 0 || n > 9) {
     return true;
   } else {
     return false;
-  }
-}
-
-function sameArray(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (var i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-function removeFlagged(arr, tile) {
-  for (var i = 0; i < arr.length; i++) {
-    var currentTile = arr[i];
-    if (currentTile.coord === tile.coord) {
-      arr = arr.slice(0, i).concat(arr.slice(i + 1, arr.length));
-    }
   }
 }
 
